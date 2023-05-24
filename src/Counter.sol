fe37ffbe927fd10c184f8471bd94d7309497416b
mustythefirst <m.adde.osman@gmail.com>
2023-05-24 07:01:13 +0100

    https://github.com/mustythefirst/Foundry-withCorrectConfig.git

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
 contract Counter {
     uint256 public number;
 
